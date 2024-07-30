6c3fc28dc64e61bd52e99d4f85bedfde09fdce8f
R-unic <roleyspersonal@gmail.com>
2024-07-29 22:18:18 -0400

    feat: initial commit

---
+++ src/Server/Main.server.cs
@@ -0,0 +1,12 @@
+using Shared;
+
+namespace Server
+{
+    public static class Game
+    {
+        public static void Main()
+        {
+            Hello.SayHello();
+        }
+    }
+}
---
+++ src/Shared/Hello.cs
@@ -0,0 +1,10 @@
+namespace Shared
+{
+    public static class Hello
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello C#!");
+        }
+    }
+}
